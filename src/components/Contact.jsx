1e1b3de05833662b5a58bef53697337915da75fb
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2023-02-08 12:20:32 -0300

    (refactor) new structure contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,31 +10,27 @@ export default function Contact() {
 
         <div className={style.contact_container} id='contact'>
 
-            <p>Feel free to contact me if you are interested!</p>
+            <p>Feel free to contact me if you are interested! <span className={style.twinkle}>Click to follow the link</span></p>
 
-            <p className={style.twinkle}>Click to follow the link</p>
+            <div className={style.social}>
+                
+                <a href='https://github.com/BelenDeAmorrortu' target='_blank'><div>
+                    <img src={Github} alt='Github Logo' />
+                    <p>BelenDeAmorrortu</p>
+                </div></a>
+                
+                <a href='https://linkedin.com/in/belen-de-amorrortu' target='_blank'><div>
+                    <img src={LinkedIn} alt='Linked Logo' />
+                    <p>Belén de Amorrortu</p>
+                </div></a>
 
-            <a href='https://github.com/BelenDeAmorrortu' target='_blank'>
-            <div>
-                <img src={Github} alt='Github Logo' />
-                <h3>BelenDeAmorrortu</h3>
+                <a href="mailto:belendeamorrortu.developer@gmail.com"><div className={style.full}>
+                    <img src={Email} alt='Email Logo' />
+                    <p>belendeamorrortu.developer@gmail.com</p>
+
+                </div></a>
+
             </div>
-            </a>
-
-            <div>
-                <img src={Email} alt='Email Logo' />
-                <a href="mailto:belendeamorrortu.developer@gmail.com">
-                    <h3>belendeamorrortu.developer@gmail.com</h3>
-                </a>
-            </div>
-
-            <a href='https://linkedin.com/in/belen-de-amorrortu' target='_blank'>
-            <div>
-                <img src={LinkedIn} alt='Linked Logo' />
-                <h3>Belén de Amorrortu</h3>
-            </div>
-            </a>
-
         
         </div>
     )
